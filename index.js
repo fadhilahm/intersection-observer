1bead70fb891ad4112372919012b09d07b258f10
fadhilahm <fadhilahmetra@gmail.com>
2022-09-06 15:02:37 +0900

    Created infinite scrolling behaviour.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const appearObserver = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       entry.target.classList.toggle("show", entry.isIntersecting);
+      //   appearObserver.unobserve(entry.target); // This will delete the observation.
+      if (entry.isIntersecting) appearObserver.unobserve(entry.target); // Use this to animate the appear sequence only.
     });
   },
   {
@@ -13,3 +15,33 @@ const appearObserver = new IntersectionObserver(
 );
 
 cards.forEach((card) => appearObserver.observe(card));
+
+const lastCard = document.querySelector(".card:last-child");
+
+const infiniteScrollObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return; // Remember to handle `isIntersecting` so it will behave properly.
+      loadNewCards();
+      infiniteScrollObserver.unobserve(entry.target);
+      const lastCard = document.querySelector(".card:last-child");
+      infiniteScrollObserver.observe(lastCard);
+    });
+  },
+  {
+    rootMargin: "200px", // Emulate API call
+  }
+);
+
+infiniteScrollObserver.observe(lastCard);
+
+const loadNewCards = () => {
+  const cardContainer = document.querySelector(".card-container");
+  for (let i = 0; i < 10; i++) {
+    const card = document.createElement("div");
+    card.textContent = "New Card";
+    card.classList.add("card");
+    appearObserver.observe(card);
+    cardContainer.append(card);
+  }
+};
